bc4fe45529db617f0e45e1b8870b8abefbbc50c3
Michał Urbaniak <blali733@gmail.com>
2018-05-15 16:41:08 +0200

    Initial Commit

---
+++ CommonStructures.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MyNetworkConfig
+{
+    public string address;
+    public string port;
+    public MyNetworkConfig(string address, string port)
+    {
+        this.address = address;
+        this.port = port;
+    }
+}
+
+public struct TestCase
+{
+    public int targets;
+    public int 
+}
