fc14cb360d43ca04dcfdf55f4e5185550a190f98
Michał Urbaniak <blali733@gmail.com>
2018-05-20 10:55:00 +0200

    Implemented basic network messages.

--- CommonHelpers.cs
+++ CommonHelpers.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace SharedTypes
 {
@@ -16,6 +18,11 @@ namespace SharedTypes
 
                 yield return value;
             }
+        }
+
+        public static float Distance2D(float x1, float y1, float x2, float y2)
+        {
+            return (float) Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
         }
     }
 }
--- SharedMessages.cs
+++ SharedMessages.cs
@@ -1,9 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using SharedTypes;
+using UnityEngine.Networking;
 
 namespace SharedMessages
 {
+    public class MyMsgType
+    {
+        public static short TestCases = MsgType.Highest + 1;
+        public static short TargetDatas = MsgType.Highest + 1;
+        public static short TargetInfos = MsgType.Highest + 1;
+    }
 
+    public class TestCasesMessage : MessageBase
+    {
+        public List<TestCase> TestCases;
+
+        public TestCasesMessage(List<TestCase> testCases)
+        {
+            TestCases = testCases;
+        }
+
+        public TestCasesMessage() { }
+    }
+
+    public class RawTargetDatasMessage : MessageBase
+    {
+        public List<TargetData> TargetDatas;
+
+        public RawTargetDatasMessage(List<TargetData> targetDatas)
+        {
+            TargetDatas = targetDatas;
+        }
+
+        public RawTargetDatasMessage() { }
+    }
+
+    public class TargetInfosMessage : MessageBase
+    {
+        public List<TargetInfo> TargetInfos;
+
+        public TargetInfosMessage(List<TargetInfo> targetInfos)
+        {
+            TargetInfos = targetInfos;
+        }
+
+        public TargetInfosMessage() { }
+    }
+
+    public struct TargetInfo
+    {
+        public float PixelDistance;
+        public float UnitDistance;
+        public float UnitSize;
+        public int PixelSize;
+        public TimeSpan Duration;
+        public Color Color;
+
+        public TargetInfo(float pixelDistance, float unitDistance, float unitSize, int pixelSize, TimeSpan duration, Color color)
+        {
+            PixelDistance = pixelDistance;
+            UnitDistance = unitDistance;
+            UnitSize = unitSize;
+            PixelSize = pixelSize;
+            Duration = duration;
+            Color = color;
+        }
+
+        public TargetInfo(TargetData previousTarget, TargetData currentTarget)
+        {
+            PixelDistance = Helpers.Distance2D(previousTarget.XPixelPosition, previousTarget.YPixelPosition,
+                currentTarget.XPixelPosition, currentTarget.YPixelPosition);
+            UnitDistance = Helpers.Distance2D(previousTarget.XUnitPosition, previousTarget.YUnitPosition,
+                currentTarget.XUnitPosition, currentTarget.YUnitPosition);
+            UnitSize = currentTarget.UnitSize;
+            PixelSize = currentTarget.PixelSize;
+            Duration = currentTarget.Duration;
+            Color = currentTarget.Color;
+        }
+    }
 }
